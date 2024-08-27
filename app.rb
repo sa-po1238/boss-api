b088e6b61da93ee4d72b7b3d8be4baad1da9bcbb
EC2 Default User <ec2-user@ip-10-1-9-18.ap-northeast-1.compute.internal>
2024-08-27 17:13:01 +0000

    ミッションまで

--- app.rb
+++ app.rb
@@ -7,3 +7,16 @@ require "httparty"
 get '/' do
   erb :index
 end
+
+get '/search' do
+    keyword = params[:keyword]
+    
+    query = {term: keyword, country: 'jp', media: 'music', limit: 6}
+    response = HTTParty.get('https://itunes.apple.com/search', query: query)
+    
+    returned_json = JSON.parse(response.body)
+    
+    @musics = returned_json['results']
+    
+    erb :result
+end
